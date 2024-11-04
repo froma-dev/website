5a38f8ce89ea8dacd2bcd7d4daf2fd90c084d140
froma <github@froma.dev>
2024-11-04 20:21:04 +0100

    add: jobs page

---
+++ src/staticpaths/jobs.ts
@@ -0,0 +1,30 @@
+import type {Job} from "@types/Job.ts";
+
+export const JOBS: Job[] = [
+    {
+        id: '24i',
+        name: '24i',
+        linkedinUrl: 'https://www.linkedin.com/company/24i',
+        websiteUrl: 'https://www.24i.com/',
+        roles: ['SmartTV Developer', 'Team Leader '],
+        tags: ['Samsung Tizen', 'LG WebOS', 'AndroidTV', 'OTT'],
+        description: 'Delivered seamless VOD and Live streaming experiences including Catch-Up, Start-Over, and CDVR, ' +
+            'leveraging DRMs and different players (Shaka Player and native TV players).',
+        logo: '/images/24i_logo.svg',
+        backgroundUrl: 'https://res.cloudinary.com/fromaweb/image/upload/e_grayscale/images/cthefogvyyi6z9ulvqad.jpg'
+    },
+    {
+        id: 'nokia',
+        name: 'Nokia TECSS',
+        linkedinUrl: 'https://www.linkedin.com/company/nokia-tecss',
+        websiteUrl: 'https://www.nokia.com/',
+        roles: ['Frontend Developer'],
+        tags: ['Samsung Tizen', 'LG WebOS', 'Arrys STB', 'IPTV'],
+        description: 'A skilled frontend developer, known for building top-tier streaming apps that delighted users ' +
+            'across Europe, North America, and South America, also excelled as a team lead. Simultaneously guiding ' +
+            'colleagues and crafting exceptional user experiences, this developer proves that technical brilliance, ' +
+            'leadership skills, and a global perspective can thrive together, leading to remarkable outcomes.',
+        logo: '/images/nokia_logo.svg',
+        backgroundUrl: 'https://res.cloudinary.com/fromaweb/image/upload/e_grayscale/images/cthefogvyyi6z9ulvqad.jpg'
+    }
+]
---
+++ src/types/Job.ts
@@ -0,0 +1,11 @@
+export interface Job {
+    id: string,
+    name: string,
+    linkedinUrl: string,
+    websiteUrl: string,
+    roles: string[]
+    tags: string[]
+    description: string,
+    logo: string,
+    backgroundUrl: string
+}
