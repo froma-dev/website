2e5f002572002fc793744a929f66491caa02e9a4
froma <github@froma.dev>
2024-11-22 00:52:11 +0100

    change: AWS base url to match cloudflare Cloud Connector rule

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
     CLOUDINARY_BASE_URL: "https://res.cloudinary.com/fromaweb/",
-    AWS_S3_BASE_URL: "https://s3.eu-north-1.amazonaws.com/froma.dev/",
+    AWS_S3_BASE_URL: "https://froma.dev/froma.dev/images/",
     DEFAULT_CLOUD: 's3'
 }
 
