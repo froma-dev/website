2acfcf5c508fba3f7c41ee468b3c3db906143874
froma <github@froma.dev>
2025-01-22 02:03:30 +0100

    change: getObjectUrl to getImageUrl

--- src/staticpaths/jobs.ts
+++ src/staticpaths/jobs.ts
@@ -1,7 +1,7 @@
 import type {Job} from "../types/Job.ts";
-import {getObjectUrl} from "@services/cloud/cloud.ts";
-const background24i = getObjectUrl('ams_canals.webp').toString()
-const backgroundNokia = getObjectUrl('nokia.webp').toString()
+import {getImageUrl} from "@services/cloud/cloud.ts";
+const background24i = getImageUrl('ams_canals.webp').toString()
+const backgroundNokia = getImageUrl('nokia.webp').toString()
 
 export const JOBS: Job[] = [
     {
