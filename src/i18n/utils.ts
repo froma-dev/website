a713439bbf6d59b152d760ea4a4beacdfd7d35d5
froma <github@froma.dev>
2025-01-26 00:36:09 +0100

    change: language selection component WIP

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,8 +1,27 @@
-import { locales, defaultLang, languages } from './locales.ts';
+import {defaultLang, languages, locales} from './locales.ts';
+
 let currentUrl: URL | undefined
 
-export function getLanguages (){
+export function getLanguages() {
     return languages
+}
+
+export function getTranslatedUrl(fromLanguage: string, toLanguage: string, url: URL) {
+    const isCurrentLanguage = fromLanguage === toLanguage
+    const {pathname, origin} = url
+    let path = pathname
+    const parts = pathname?.split('/')
+
+    console.log(pathname, fromLanguage, toLanguage, defaultLang)
+    if (fromLanguage !== defaultLang) {
+        parts.shift() || parts.shift()
+        path = `/${toLanguage}${parts.join('/')}`
+
+
+        console.log('-EEEE-------', new URL(path, origin).href)
+    }
+
+    return new URL(path, origin)
 }
 
 export function getLangFromUrl(url: URL | undefined) {
