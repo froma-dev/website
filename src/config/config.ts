f6bd138ad23074f4da62ae0bb1e615b68c193165
froma <github@froma.dev>
2025-01-26 20:04:55 +0100

    change: change aws s3 bucket name

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
     CLOUDINARY_BASE_URL: "https://res.cloudinary.com/fromaweb/",
-    AWS_S3_IMAGES_BASE_URL: "https://froma.dev/froma.dev/images/",
-    AWS_S3_DOCUMENTS_BASE_URL: "https://froma.dev/froma.dev/documents/",
+    AWS_S3_IMAGES_BASE_URL: "https://froma.dev/froma-dev/images/",
+    AWS_S3_DOCUMENTS_BASE_URL: "https://froma.dev/froma-dev/documents/",
     DEFAULT_CLOUD: 's3'
 }
 
