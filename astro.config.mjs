31cd351a0e013b7d80615d24ab3818f86324f8f8
froma <github@froma.dev>
2025-02-11 03:23:04 +0100

    change: New domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://froma.dev',
+  site: 'https://frankromana.dev',
   devToolbar: {enabled: false},
   integrations: [sitemap()],
   i18n: {
