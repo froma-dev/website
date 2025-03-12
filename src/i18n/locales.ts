8493f7e66dc29e48b66fe31c01d03ba7cedd9f5f
froma <github@froma.dev>
2025-03-12 23:21:07 +0100

    fix: adjust years to professional 9y6m

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -7,7 +7,7 @@ export const defaultLang = 'en'
 
 export const locales = {
     en: {
-        'main.metadata.description': `Meet Frank Romaña. With over 10 years as a front-end developer, Frank has transformed screens into
+        'main.metadata.description': `Meet Frank Romaña. With over 9 years as a front-end developer, Frank has transformed screens into
                 captivating, user-focused experiences. Known for his ability to lead and innovate, Frank seamlessly
                 blends design with technology, creating applications that continually elevate user expectations.
                 The journey continues with every new line of code.`,
@@ -19,9 +19,9 @@ export const locales = {
         'main.metadata.button.playPause.ariaLabel': 'play or pause',
         'main.metadata.button.mute.ariaLabel': 'mute or unmute',
         'main.metadata.button.downloadCv': 'Download CV',
-        'meta.description': 'Frank Romaña is a software engineer with over a decade of professional experience building user-friendly and high-performance web applications. He is passionate about crafting intuitive interfaces and delivering robust, scalable solutions.',
+        'meta.description': 'Frank Romaña is a software engineer with over 9 years of professional experience building user-friendly and high-performance web applications. He is passionate about crafting intuitive interfaces and delivering robust, scalable solutions.',
         'meta.title': 'Frank Romaña - Front-End Developer | Web Developer | Software Engineer',
-        'experience.meta.description': 'Frank Romaña is a software engineer with over a decade of professional experience building user-friendly and high-performance web applications. He is passionate about crafting intuitive interfaces and delivering robust, scalable solutions.',
+        'experience.meta.description': 'Frank Romaña is a software engineer with over 9 years of professional experience building user-friendly and high-performance web applications. He is passionate about crafting intuitive interfaces and delivering robust, scalable solutions.',
         'experience.meta.title': 'Frank Romaña - Experience at ',
         'main.background.alt': 'Frank Romaña - Developer',
         'experience.tag.seasons': 'Seasons',
@@ -34,13 +34,13 @@ export const locales = {
         'select.language.arialabel': 'change language',
         'asset.link.arialabel': 'Learn more about Frank\'s experience at',
         'not.found.404.title': '404 Not Found',
-        'not.found.404.description': '404 Not Found - Frank Romaña is a software engineer with over a decade of professional experience building user-friendly and high-performance web applications. He is passionate about crafting intuitive interfaces and delivering robust, scalable solutions.',
+        'not.found.404.description': '404 Not Found - Frank Romaña is a software engineer with over 9 years of professional experience building user-friendly and high-performance web applications. He is passionate about crafting intuitive interfaces and delivering robust, scalable solutions.',
         'not.found.404.message.title': 'No Signal (404)',
         'not.found.404.home.button.text': 'Back to Home',
         'experience.carousel.title': 'Experience'
     },
     es: {
-        'main.metadata.description': `Con más de 10 años como desarrollador front-end, Frank transforma pantallas en experiencias atractivas y
+        'main.metadata.description': `Con más de 9 años como desarrollador front-end, Frank transforma pantallas en experiencias atractivas y
         centradas en el usuario. Su habilidad para innovar y liderar combina diseño y tecnología, creando
         aplicaciones que superan expectativas.
         La historia sigue con cada línea de código.`,
@@ -52,7 +52,7 @@ export const locales = {
         'main.metadata.button.playPause.ariaLabel': 'reproducir o pausar',
         'main.metadata.button.mute.ariaLabel': 'silenciar o sin silenciar',
         'main.metadata.button.downloadCv': 'Descargar CV',
-        'meta.description': 'Frank Romaña es un ingeniero de software con más de una década de experiencia profesional en el desarrollo de aplicaciones web de alto rendimiento y orientadas al usuario. Su pasión radica en la creación de interfaces intuitivas y en la entrega de soluciones robustas y escalables que cumplen con los más altos estándares de calidad.',
+        'meta.description': 'Frank Romaña es un ingeniero de software con más 9 años de experiencia profesional en el desarrollo de aplicaciones web de alto rendimiento y orientadas al usuario. Su pasión radica en la creación de interfaces intuitivas y en la entrega de soluciones robustas y escalables que cumplen con los más altos estándares de calidad.',
         'meta.title': 'Frank Romaña - Desarrollador Front-End | Desarrollador Web | Ingeniería de Software',
         'experience.meta.description': 'Frank Romaña es un ingeniero de software con más de una década de experiencia profesional en el desarrollo de aplicaciones web de alto rendimiento y orientadas al usuario. Su pasión radica en la creación de interfaces intuitivas y en la entrega de soluciones robustas y escalables que cumplen con los más altos estándares de calidad.',
         'experience.meta.title': 'Frank Romaña - Experiencia en ',
